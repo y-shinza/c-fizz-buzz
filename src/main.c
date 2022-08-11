6aaf5b6f21350657c4b4b35a2317588f5db78498
Shinza Yoshiya <real.shinza@gmail.com>
2022-08-11 23:49:42 +0900

    Initial commit

---
+++ src/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void main()
+{
+    int i, count;
+
+    printf("ŒJ‚è•Ô‚·‰ñ”‚ğ“ü—Í‚µ‚Ä‰º‚³‚¢B\n");
+    scanf("%d", &count);
+    
+    for (i = 1; i <= count; i++)
+    {
+        if (i % 15 == 0)
+            printf("FizzBuzz\n");
+        else if (i % 3 == 0)
+            printf("Fizz\n");
+        else if (i % 5 == 0)
+            printf("Buzz\n");
+        else
+            printf("%d\n", i);
+    }
+}
